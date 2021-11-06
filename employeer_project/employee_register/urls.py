0f80b8910507dcf3417bfbfe8f594a43a0d92e39
Terdesja <gerfranmarquez@gmail.com>
2021-11-06 06:26:25 -0700

    next

--- employeer_project/employee_register/urls.py
+++ employeer_project/employee_register/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.employee_form, name='employee_insert'),
     path('list/', views.employee_list,name='employee_list'),
+    path('delete/<int:id>/', views.employee_delete, name='employee_delete'),
     path('<int:id>/', views.employee_form, name='employee_update')
 ]
